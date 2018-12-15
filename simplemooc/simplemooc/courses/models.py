ad5914a0476b8b467ba1a4436930c88981e46101
Hugo <hugovmari@hotmail.com>
2018-12-15 09:38:12 -0200

    Admin config

--- simplemooc/simplemooc/courses/models.py
+++ simplemooc/simplemooc/courses/models.py
@@ -1,5 +1,16 @@
 from django.db import models
 
+class CourseManager(models.Manager):
+	
+	def search(self, query):
+		return self.get_queryset().filter(
+			models.Q(name__icontains=query) | \
+			models.Q(description__icontains=query)
+			)
+
+
+
+
 class Course(models.Model):
 
 	name = models.CharField('Nome', max_length=100)
@@ -7,10 +18,15 @@ class Course(models.Model):
 	description = models.TextField('Descrição', blank=True)
 	start_date = models.DateField('Data de Início', null=True, blank=True)
 	image = models.ImageField(
-			upload_to='courses/images', verbose_name='Imagem'
+			upload_to='courses/images', verbose_name='Imagem', 
+			null=True, blank=True
 		)
 	created_at = models.DateTimeField(
 			'Criado em', auto_now_add=True
 		)
 	update_at = models.DateTimeField('Atualizado em', auto_now=True)
 
+	objects = CourseManager()
+
+	def __str__(self):
+		return self.name
